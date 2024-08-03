59680b7f2ef296969f25972bde17eab68594fd7f
Nathan Touroux <touroux.nathan@gmail.com>
2024-08-03 14:25:36 +0200

    use a (Map Sym Int) to repersent the max of the levels

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,11 @@
 module Lib (module Lib) where
 
-import Data.List ( (\\) )
+import Data.List ( (\\), intercalate )
 import Control.Monad (unless)
 import Prelude hiding (id)
 import Data.Either (fromRight)
+import Data.Map (Map, singleton, insert, delete, toList, keys)
+import qualified Data.Map as Map
 
 type Sym = String
 data Expr
@@ -11,7 +13,7 @@ data Expr
     | Expr :@ Expr -- Application
     | Expr ::> Expr -- Left typing: Type ::> term
     | (Sym, Expr) :-> Expr -- Dependent lambda
-    | U Sym Int -- Universe
+    | Um (Map Sym Int) -- Universe where the level is the max of each Sym+Int
     | L -- Type for unvirse level
     deriving (Eq, Read)
 infixr 2 :->
@@ -27,18 +29,28 @@ instance Show Expr where
         | t == S "" = s ++ " -> " ++ show e
         | otherwise = "(" ++ s ++ " : " ++ show t ++ ") -> " ++ show e
     show (t ::> e) = show t ++ " :> " ++ show e
-    show (U s i)
-        | null s = "U " ++ show i
-        | i == 0 = "U " ++ s
-        | otherwise = "U " ++ s ++ "+" ++ show i
+    show (Um l) = "U " ++ showL l
     show L = "L"
+
+showL :: Map Sym Int -> String
+showL m = showm (toList m)
+    where
+        showm [] = ""
+        showm [(l, i)] = showU l i
+        showm lis = "max(" ++ intercalate "," (uncurry showU <$> lis) ++ ")"
+
+showU :: Sym -> Int -> String
+showU l i
+    | null l =  show i
+    | i == 0 =  l
+    | otherwise =  l ++ "+" ++ show i
 
 freeVars :: Expr -> [Sym]
 freeVars (S s) = [s]
 freeVars (f :@ a) = freeVars f ++ freeVars a
 freeVars (t ::> e) = freeVars e ++ freeVars t
 freeVars ((s, _t) :-> e) = freeVars e \\ [s]
-freeVars (U l _) = [l]
+freeVars (Um ls) = keys ls
 freeVars L = []
 
 whnf :: Expr -> Expr
@@ -62,8 +74,10 @@ subst s x = sub
                 e'' = substVar s' s'' e'
             in (s'', sub t') :-> sub e''
           | otherwise = (s', sub t') :-> sub e'
-        sub ul@(U l i) = case x of
-            S l' -> if s == l then U l' i else ul
+        sub ul@(Um ls) = case x of
+            S l -> case Map.lookup s ls of
+                Just i -> Um (insert l i $ delete s ls)
+                Nothing -> ul
             _ -> ul
         sub L = L
 
@@ -81,7 +95,7 @@ alphaEq (S s) (S s') = s == s'
 alphaEq (f :@ x) (f' :@ x') = alphaEq f f' && alphaEq x x'
 alphaEq (t ::> e) (t' ::> e') = alphaEq e e' && alphaEq t t'
 alphaEq ((s, t) :-> e) ((s', t') :-> e') = alphaEq e (substVar s' s e') && alphaEq t t'
-alphaEq (U l i) (U l' i') = l == l' && i == i'
+alphaEq (Um ls) (Um ls') = ls == ls' -- FIXME: actually when used in tCheck we want a directional operator that checks that all the elements of ls must be in ls' and they should have a smaller or equal Int
 alphaEq L L = True
 alphaEq _ _ = False
 
@@ -104,7 +118,7 @@ erased (S s) = S s
 erased (f :@ x) = erased f :@ erased x
 erased (_t ::> e) = erased e
 erased ((s, _t) :-> e) = (s, S "") :-> erased e
-erased (U l i) = U l i
+erased (Um ls) = Um ls
 erased L = L
 
 erasedBetaEq :: Expr -> Expr -> Bool
@@ -148,22 +162,24 @@ tCheck env ((s, t) :-> e) = do
     let env' = extend env s t
     te <- tCheck env' e
     return $ (s, t) :-> te
-tCheck _ (U l i) = return $ U l (i+1)
-tCheck _ L = return $ U "" 0
+tCheck _ (Um ls) = return $ Um ((+1) <$> ls)
+tCheck _ L = return $ Um (singleton "" 0)
 
-universe :: Env -> Expr -> TC (Sym, Int)
-universe _ L = return ("", 0)
-universe _ (U l i) = return (l, i+1)
+universe :: Env -> Expr -> TC (Map Sym Int)
+universe _ L = return $ singleton "" 0
+universe _ (Um ls) = return ((+1) <$> ls)
 universe env (S s) = findVar env s >>= universe env
 universe env (f :@ _) = universe env f
 universe env (_ ::> e) = universe env e
-universe env ((s, t) :-> e) = universe (extend env s t) e
+universe env ((s, t) :-> e) = universe (extend env s t) e -- FIXME: check that this is correct
 
 typeCheck :: Expr -> TC Expr
 typeCheck = tCheck initialEnv
 
 u :: Int -> Expr
-u = U ""
+u i = Um (singleton "" i)
+us :: Sym -> Int -> Expr
+us l i = Um (singleton l i)
 
 id :: Expr
 id = ("t", u 1) :-> ("x", S "t") :-> S "t" ::> S "x"
@@ -174,7 +190,7 @@ higher = ("f", ("t", u 1) :-> ("", S "t") :-> S "t") :-> S "f"
 bigger :: Expr
 bigger = ("f", ("t", u 1) :-> ("r", u 4) :-> S "r") :-> S "f"
 level :: Expr
-level = ("i", L) :-> ("T", U "i" 0) :-> ("t", S "T") :-> S "T" ::> S "t"
+level = ("i", L) :-> ("T", us "i" 0) :-> ("t", S "T") :-> S "T" ::> S "t"
 
 zero :: Expr
 zero = ("a", u 1) :-> ("b", u 1) :-> ("s", S "a") :-> ("z", S "b") :-> S "z"
@@ -188,7 +204,7 @@ showLam :: Sym -> Expr -> IO ()
 showLam s lam = let lam' = nf lam in
      case typeCheck lam' of
          Right t -> do
-             putStrLn $ s ++ " :: " ++ show t ++ " | " ++ show (case fromRight ("", -1) (universe initialEnv t) of (l, i) -> U l i)
+             putStrLn $ s ++ " :: " ++ show t ++ " | " ++ show (case fromRight (singleton "" (-1)) (universe initialEnv t) of ls -> Um ls)
              putStrLn $ s ++ " = " ++ show (erased lam')
          Left err -> print err
 
